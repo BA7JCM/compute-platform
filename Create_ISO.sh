a30bad879b465ae349be812958897dcdc3989614
BA7JCM <1677128936@qq.com>
2023-03-29 14:12:19 +0800

    auto check disk type and name

--- Create_ISO.sh
+++ Create_ISO.sh
@@ -1,3 +1,5 @@
+# message : this script only run at centos stream with legacy boot model can package normal iso file
+# todo : can package normal iso file on any rhel base system
 dnf install rsync createrepo genisoimage wget gzip -y
 # get scripts dir
 work_dir=$(cd "$(dirname "$0")" && pwd)
@@ -8,8 +10,8 @@ repo_base_url="centos/8-stream/"
 repo_domain="https://repo.huaweicloud.com/"
 exfat_base_url="https://download1.rpmfusion.org/free/el/updates/8/x86_64/"
 # check iso exist or not,if not,download it from huawei mirror
-if [ ! -f "./CentOS-Stream-8-x86_64-latest-dvd1.iso" ]; then
-
+if [ ! -f "$work_dir/CentOS-Stream-8-x86_64-latest-dvd1.iso" ]; then
+	echo "$work_dir/CentOS-Stream-8-x86_64-latest-dvd1.iso not exist"
 	wget https://repo.huaweicloud.com/centos/8-stream/isos/x86_64/CentOS-Stream-8-x86_64-latest-dvd1.iso --user-agent=" Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0"
 
 else
@@ -25,11 +27,11 @@ if [ $mount_check -eq 0 ]; then
 	}
 else
 	{
-		umount ./CentOS-Stream-8-x86_64-latest-dvd1.iso -fl
+		umount $work_dir/CentOS-Stream-8-x86_64-latest-dvd1.iso -fl
 	}
 fi
 
-mount ./CentOS-Stream-8-x86_64-latest-dvd1.iso /media -r
+mount $work_dir/CentOS-Stream-8-x86_64-latest-dvd1.iso /media -r
 mkdir /iso
 
 rsync -av --progress /media/ /iso/ --exclude BaseOS --exclude AppStream
@@ -57,9 +59,8 @@ mkdir -p /iso/ExtraPackages/Packages/
 cd /iso/ExtraPackages/
 # add some package to support more thing
 echo download package
-# backup :    --repofrompath=centos_appstream,/media/AppStream --repofrompath=centos_baseos,/media/BaseOS 
-dnf -4 download --disablerepo="*" --repofrompath=centos_appstream,${repo_domain}${repo_base_url}AppStream/x86_64/os/ --repofrompath=centos_baseos,${repo_domain}${repo_base_url}BaseOS/x86_64/os/ --repofrompath=centos_epel,${repo_domain}epel/8/Everything/x86_64 --repofrompath=centos_el,${exfat_base_url} --releasever=8  -y --resolve --downloadonly --downloaddir=/iso/ExtraPackages/Packages/ --alldeps genisoimage libusal qemu-kvm libvirt virt-install cockpit cockpit-machines bash-completion vim grub2-efi-x64 dosfstools efi-filesystem efibootmgr shim-x64 pciutils usbutils util-linux net-tools ntfs-3g kernel-modules-extra exfat-utils fuse-exfat python39  python3-pyyaml
-# find /iso/ -name NetworkManager*.rpm | xargs rm
+# backup :    --repofrompath=centos_appstream,/media/AppStream --repofrompath=centos_baseos,/media/BaseOS --alldeps
+dnf -4 download --disablerepo="*" --repofrompath=centos_appstream,${repo_domain}${repo_base_url}AppStream/x86_64/os/ --repofrompath=centos_baseos,${repo_domain}${repo_base_url}BaseOS/x86_64/os/ --repofrompath=centos_epel,${repo_domain}epel/8/Everything/x86_64 --repofrompath=centos_el,${exfat_base_url} --releasever=8  -y --resolve --downloadonly --downloaddir=/iso/ExtraPackages/Packages/  genisoimage libusal qemu-kvm libvirt virt-install cockpit cockpit-machines bash-completion vim grub2-efi-x64  efi-filesystem efibootmgr shim-x64 exfat-utils fuse-exfat
 if [ $? -eq 0 ]; then
 	echo download complete
 else
